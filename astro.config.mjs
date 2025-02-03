1b94406e408816869628ebd932923d8af0e464ad
Patrick Soo <patsoo99@gmail.com>
2025-02-03 00:14:04 -0500

    allow host

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,21 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      host: true, // Allows external access (0.0.0.0)
+      port: 4321, // Ensure Astro runs on the correct port
+      strictPort: false, // Allows other ports if 4321 is taken
+    },
+    preview: {
+      allowedHosts: ["patricksoo.me"], // Allow your domain
+    },
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });

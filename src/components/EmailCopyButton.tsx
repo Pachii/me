32b1cd4ae1438a833a171b6f64ac8e6faca47b8e
Patrick Soo <patsoo99@gmail.com>
2025-02-02 20:48:26 -0500

    more homepage stuff + react

---
+++ src/components/EmailCopyButton.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+interface Props {
+  email: string;
+}
+
+export default function ContactButton({ email }: Props) {
+  const [copied, setCopied] = useState(false);
+
+  const copyEmail = () => {
+    navigator.clipboard.writeText(email).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+    });
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      {/* Email Icon using Iconify */}
+      <span
+        className="iconify"
+        data-icon="mdi:email-outline"
+        data-inline="false"
+      ></span>
+      {/* Click-to-copy button */}
+      <button
+        type="button"
+        onClick={copyEmail}
+        className="cursor-pointer hover:bg-sky-200 transition-colors duration-300 focus:outline-none"
+      >
+        {copied ? email + " (copied)" : email}
+      </button>
+    </div>
+  );
+}
